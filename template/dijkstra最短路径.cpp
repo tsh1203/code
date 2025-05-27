b80c90b6cd9449487c115aec4fbc56617f4ab93d
tsh1203 <David_tsh@163.com>
2025-05-27 19:43:51 +0800

    算法模板

---
+++ template/dijkstra最短路径.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define pai pair<int,int>
+using namespace std;
+struct edge
+{
+	int v,w,next;
+}e[200005];
+int n,m,s,t;
+int x[102],y[102];
+int head[200005],idx;
+int dis[100005],vis[100005],sum,ans[100005];
+priority_queue<pai,vector<pai>,greater<pai>>q;
+void add(int u,int v,int w)
+{
+	e[++idx].v=v;
+	e[idx].w=w;
+	e[idx].next=head[u];
+	head[u]=idx;
+}
+void dij()
+{
+	memset(dis,127,sizeof(dis));
+	dis[s]=0;
+	q.push(make_pair(dis[1],1));
+	while(!q.empty())
+	{
+		int u=q.top().second;
+		q.pop();
+		if(vis[u])
+			continue;
+		vis[u]=1;
+		for(int i=head[u];i;i=e[i].next)
+		{
+			int v=e[i].v;
+			if(dis[v]>dis[u]+e[i].w)
+			{
+				dis[v]=dis[u]+e[i].w;
+				q.push(make_pair(dis[v],v));
+			}
+		}
+	}
+}
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>n>>m>>s>>t;
+	for(int i=1;i<=m;i++)
+	{
+		int a,b,l;
+		cin>>a>>b>>l;
+		add(a,b,l);
+	}
+	dij();
+	cout<<dis[t];
+	return 0;
+}
---
+++ template/组合数.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("3","Ofast","inline")
+#define int long long
+#define endl "\n"
+using namespace std;
+const int mod=998244353;
+int r,g,b,k,ans;
+int mi(int a,int b)
+{
+	int ans=1;
+	while(b)
+	{
+		if(b%2)ans*=a,ans%=mod;
+		a=a*a%mod;
+		b/=2;
+	}
+	return ans;
+}
+int C(int a,int b)
+{
+	if(a==0)return 0;
+	if(b==0||b==a)return 1;
+	if(b>a/2)b=a-b;
+	int ans=1;
+	for(int i=a;i>=a-b+1;i--)
+	{
+		ans*=i,ans%=mod;
+	}
+	for(int i=1;i<=b;i++)
+	{
+		ans*=mi(i,mod-2);
+		ans%=mod;
+	}
+	return ans;
+}
+signed main()
+{
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+	int x,y;
+	cin>>x>>y;
+	cout<<C(x,y);
+	return 0;
+}
