d8660efb754cdc251ff64bfeb26e187070e2d5e5
tsh_qwq <David_tsh@163.com>
2025-05-29 13:07:51 +0800

    Add files via upload

---
+++ Template/基本模板.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("3","Ofast","inline")
+#define int long long
+#define endl "\n"
+using namespace std;
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	return 0;
+}
