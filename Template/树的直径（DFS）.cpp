981c6c09d0d487fa278b497e7b8990c0a295a056
tsh1203 <David_tsh@163.com>
2025-06-03 14:58:25 +0800

    树的直径

---
+++ Template/树的直径（DFS）.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+int n,c,d[100005];
+vector<pair<int,int>>e[100005];
+void dfs(int u,int fa)
+{
+	for(auto x:e[u])
+	{
+		int v=x.first,w=x.second;
+		if(v==fa)continue;
+		d[v]=d[u]+w;
+		if(d[v]>d[c])c=v;
+		dfs(v,u);
+	}
+} 
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>n;
+	for(int i=1;i<n;i++)
+	{
+		int u,v,w;
+		cin>>u>>v>>w;
+		e[u].push_back({v,w});
+		e[v].push_back({u,w});
+	}
+	dfs(1,0);
+	d[c]=0;
+	dfs(c,0);
+	cout<<d[c];
+	return 0;
+}
