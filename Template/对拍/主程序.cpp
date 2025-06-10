b3b011e2fd888e3c7583021b67d89fe57d9ef073
tsh1203 <David_tsh@163.com>
2025-06-10 16:54:45 +0800

    Update 主程序.cpp

--- Template/对拍/主程序.cpp
+++ Template/对拍/主程序.cpp
@@ -6,14 +6,25 @@ int main()
 	system("@echo off"); 
 	system("cd C:\\Users\\Administrator\\Desktop");//填入文件目录
 	
-	system("g++ -o std.exe  std.cpp");//原程序  
-	cout<<"std.cpp Running completed\n";
+	if (system("g++ -O3 -o std.exe   std.cpp") != 0){ 
+		cerr<<"Error: Compilation of std.cpp  failed."<< endl; 
+		return 1; 
+	} 
+	cout<<"std.cpp  Running completed\n"; 
 	
-	system("g++ -o baoli.exe  baoli.cpp");//暴力
-	cout<<"baoli.cpp Running completed\n";
+	// 编译暴力程序 
+	if (system("g++ -O3 -o baoli.exe   baoli.cpp") != 0){ 
+		cerr<<"Error: Compilation of baoli.cpp  failed."<<endl; 
+		return 1; 
+	} 
+	cout<<"baoli.cpp  Running completed\n"; 
 	
-	system("g++ -o data.exe  data.cpp");//数据生成器
-	cout<<"data.cpp Running completed\n";
+	// 编译数据生成器 
+	if (system("g++ -O3 -o data.exe   data.cpp") != 0){ 
+		cerr<<"Error: Compilation of data.cpp  failed."<<endl; 
+		return 1; 
+	} 
+	cout<<"data.cpp Running completed\n"; 
 	
 	while (1) 
 	{ 
