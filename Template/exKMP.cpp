e4a78d1011a51ef211e087c2b789ae01d347d5cc
tsh_qwq <David_tsh@163.com>
2025-07-22 19:30:28 +0800

    Update exKMP.cpp

--- Template/exKMP.cpp
+++ Template/exKMP.cpp
@@ -2,26 +2,81 @@
 #define int long long
 #define endl "\n"
 using namespace std;
-const int N=1e5+5;
-string s;
-int n,r,p,l[N];
+const int N=2e7+5;
+char s[N],ss[N],c[N*2]; 
+int z[2*N],p[N];   
 signed main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	cin>>s;
-	s=" "+s; 
-	n=s.length();
-	for(int i=2;i<=n;i++){
-		if(r>=i)
-			l[i]=min(l[i-p+1],r-i+1);
-		while(i+l[i]<=n&&s[i+l[i]]==s[l[i]+1])
-			l[i]++;
-		if(i+l[i]-1>r)
-			r=i+l[i]-1,p=i;
-	}
-	for(int i=1;i<n;i++)
-		cout<<l[i]<<" ";
-	return 0;
+    scanf("%s%s",s+1,ss+1);
+    int n=strlen(s+1),m=strlen(ss+1);
+    
+    z[1]=m;
+    int l=0,r=0;
+    for(int i=2;i<=m;i++){
+        if(i>r){
+            l=r=i;
+            while(r<=m&&ss[r]==ss[r-l+1])
+                r++;
+            z[i]=r-l;
+            r--;
+        } 
+        else{
+            int k=i-l+1;
+            if(z[k]<r-i+1)
+                z[i]=z[k];
+            else{
+                l=i;
+                while(r<=m&&ss[r]==ss[r-l+1])
+                    r++;
+                z[i]=r-l;
+                r--;
+            }
+        }
+    }
+    int zz=0;
+    for(int i=1;i<=m;i++)
+        zz^=(i*(z[i]+1));
+    printf("%lld\n",zz);
+    
+    int cl=m+n+1;
+    for(int i=1;i<=m;i++) 
+        c[i]=ss[i];
+    c[m+1]='#';  
+    for(int i=1;i<=n;i++)
+        c[m+1+i]=s[i];
+    
+    memset(z,0,sizeof(z));
+    z[1]=cl;
+    l=0,r=0;
+    for(int i=2;i<=cl;i++){
+        if(i>r){
+            l=r=i;
+            while(r<=cl&&c[r]==c[r-l+1])
+                r++;
+            z[i]=r-l;
+            r--;
+        } 
+        else{
+            int k=i-l+1;
+            if(z[k]<r-i+1)
+                z[i]=z[k];
+            else{
+                l=i;
+                while(r<=cl&&c[r]==c[r-l+1])
+                    r++;
+                z[i]=r-l;
+                r--;
+            }
+        }
+    }
+    
+    int pp=0;
+    for(int i=1;i<=n;i++)
+        pp^=(i*(z[m+1+i]+1));
+    printf("%lld",pp);
+    
+    return 0;
 }
