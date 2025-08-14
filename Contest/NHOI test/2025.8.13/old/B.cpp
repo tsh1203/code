32accc5540f6946b5f3518c25d57fc47ef055688
tsh_qwq <David_tsh@163.com>
2025-08-14 21:06:17 +0800

    Create B.cpp

---
+++ Contest/NHOI test/2025.8.13/old/B.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define int long long
+#ifdef __linux__
+#define getchar getchar_unlocked
+#define putchar putchar_unlocked
+#else
+#define getchar _getchar_nolock
+#define putchar _putchar_nolock
+#endif
+using namespace std;
+namespace FastIO {
+	void read(){}
+	template <class T1,class ... T2> inline void read(T1 &x,T2 &... oth){
+		bool pd=0;x=0;char ch=getchar();
+		while(!isdigit(ch)){pd|=ch=='-';ch=getchar();}
+		while(isdigit(ch)){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+		x=pd?-x:x;
+		read(oth...);
+	}
+	template <class T> void _write(T x){
+		if(x<0){putchar('-');x=-x;}
+		if(x>9) _write(x/10);
+		putchar(x%10^48);
+	}
+	void write(){}
+	template <class T1,class ... T2>
+	inline void write(T1 x,T2 ...oth){_write(x);putchar(' ');write(oth...);}
+	inline void endline(){putchar('\n');}
+}//Orz lby 
+using namespace FastIO;
+signed main(){
+	int n,m;
+	cin>>n>>m;
+	if(n==2&&m==4)cout<<6;
+	else if(n==3&&m==3)cout<<8;
+	else cout<<0;
+	return 0;
+}
