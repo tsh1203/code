ef99ad3b1c158878029cb3754420cd9c8f153153
tsh_qwq <David_tsh@163.com>
2025-08-23 14:57:39 +0800

    Create E.cpp

---
+++ Contest/luogu/LGR-239-Div.4/E.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define int long long
+#ifdef __linux__
+#define getchar getchar_unlocked
+#define putchar putchar_unlocked
+#else
+#define getchar _getchar_nolock
+#define putchar _putchar_nolock
+#endif
+using namespace std;
+namespace FastIO {
+	void read(){}
+	template <class T1,class ... T2> inline void read(T1 &x,T2 &... oth){
+		bool pd=0;x=0;char ch=getchar();
+		while(!isdigit(ch)){pd|=ch=='-';ch=getchar();}
+		while(isdigit(ch)){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+		x=pd?-x:x;
+		read(oth...);
+	}
+	template <class T> void _write(T x){
+		if(x<0){putchar('-');x=-x;}
+		if(x>9) _write(x/10);
+		putchar(x%10^48);
+	}
+	void write(){}
+	template <class T1,class ... T2>
+	inline void write(T1 x,T2 ...oth){_write(x);putchar(' ');write(oth...);}
+	inline void endline(){putchar('\n');}
+}//Orz lby 
+using namespace FastIO;
+const int N=1e5+5;
+int n;
+int p[N],j[N],s[N];
+int ans;
+signed main(){
+	read(n);
+	for(int i=1;i<=n;i++)read(p[i]);
+	for(int i=1;i<=n;i++)read(j[i]);
+	for(int i=1;i<=n;i++)read(s[i]);
+	for(int i=1;i<=n;i++){
+		ans+=max(0ll,s[i]-abs(p[i]-j[i]));
+	}
+	cout<<ans;
+	return 0;
+}
