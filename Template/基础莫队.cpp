bc98c5b9f4fc67956da8e4e3ae5ad49752947868
tsh_qwq <David_tsh@163.com>
2026-02-10 16:26:03 +0800

    Update 基础莫队.cpp

--- Template/基础莫队.cpp
+++ Template/基础莫队.cpp
@@ -5,30 +5,24 @@
 using namespace std;
 int n,m,now;
 int p[1000006],s[1000006],cnt[1000006],ans[1000006];
-struct no
-{
+struct no{
 	int x,y,id;
 }a[1000006];
-bool cmp(no x,no y)
-{
+bool cmp(no x,no y){
 	if(s[x.x]==s[y.x])return x.y<y.y;
 	return x.x<y.x;
 }
-void move(int x,int d)
-{
-	if(d)
-	{
+void move(int x,int d){
+	if(d){
 		if(!cnt[p[x]])now++;
 		cnt[p[x]]++;
 	}
-	else
-	{
+	else{
 		cnt[p[x]]--;
 		if(!cnt[p[x]])now--;
 	}
 }
-signed main()
-{
+signed main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
@@ -39,15 +33,13 @@ signed main()
 	int k=sqrt(n);
 	for(int i=1;i<=n;i++)
 		s[i]=(i-1)/k+1;
-	for(int i=1;i<=m;i++)
-	{
+	for(int i=1;i<=m;i++){
 		cin>>a[i].x>>a[i].y;
 		a[i].id=i;
 	}
 	sort(a+1,a+m+1,cmp);
 	int l=0,r=0;
-	for(int i=1;i<=m;i++)
-	{
+	for(int i=1;i<=m;i++){
 		while(l<a[i].x)move(l++,0);
 		while(l>a[i].x)move(--l,1);
 		while(r<a[i].y)move(++r,1);
