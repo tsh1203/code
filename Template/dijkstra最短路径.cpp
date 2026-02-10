79e6619c6d905ede695278f4586c0f37e203d3da
tsh_qwq <David_tsh@163.com>
2026-02-10 16:18:36 +0800

    Update dijkstra最短路径.cpp

--- Template/dijkstra最短路径.cpp
+++ Template/dijkstra最短路径.cpp
@@ -3,53 +3,47 @@
 #define endl "\n"
 #define pai pair<int,int>
 using namespace std;
+const int N=2e5+5;
 struct edge
 {
 	int v,w,next;
-}e[200005];
+}e[N];
 int n,m,s,t;
 int x[102],y[102];
-int head[200005],idx;
-int dis[100005],vis[100005],sum,ans[100005];
+int head[N],idx;
+int dis[N],vis[N],sum,ans[N];
 priority_queue<pai,vector<pai>,greater<pai>>q;
-void add(int u,int v,int w)
-{
+void add(int u,int v,int w){
 	e[++idx].v=v;
 	e[idx].w=w;
 	e[idx].next=head[u];
 	head[u]=idx;
 }
-void dij()
-{
+void dij(){
 	memset(dis,127,sizeof(dis));
 	dis[s]=0;
 	q.push(make_pair(dis[s],s));
-	while(!q.empty())
-	{
+	while(q.size()){
 		int u=q.top().second;
 		q.pop();
 		if(vis[u])
 			continue;
 		vis[u]=1;
-		for(int i=head[u];i;i=e[i].next)
-		{
+		for(int i=head[u];i;i=e[i].next){
 			int v=e[i].v;
-			if(dis[v]>dis[u]+e[i].w)
-			{
+			if(dis[v]>dis[u]+e[i].w){
 				dis[v]=dis[u]+e[i].w;
 				q.push(make_pair(dis[v],v));
 			}
 		}
 	}
 }
-signed main()
-{
+signed main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	cin>>n>>m>>s>>t;
-	for(int i=1;i<=m;i++)
-	{
+	for(int i=1;i<=m;i++){
 		int a,b,l;
 		cin>>a>>b>>l;
 		add(a,b,l);
