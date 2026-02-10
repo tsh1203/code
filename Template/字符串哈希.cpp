838fa90c658176a984407aa7ec0d1ea1516eda8a
tsh_qwq <David_tsh@163.com>
2026-02-10 16:26:18 +0800

    Update 字符串哈希.cpp

--- Template/字符串哈希.cpp
+++ Template/字符串哈希.cpp
@@ -7,25 +7,21 @@ const int mod=1000000007;
 int a[10004];
 int n,ans=1;
 string s;
-int hs()
-{
+int hs(){
 	int l=s.size();
 	int ans=0;
 	for(int i=0;i<l;i++)
 		ans=(ans*base+(int)s[i])%mod+prime;
 	return ans;
 }
-signed main()
-{
+signed main(){
 	cin>>n;
-	for(int i=1;i<=n;i++)
-	{
+	for(int i=1;i<=n;i++){
 		cin>>s;
 		a[i]=hs();
 	}
 	sort(a+1,a+n+1);
-	for(int i=2;i<=n;i++)
-	{
+	for(int i=2;i<=n;i++){
 		if(a[i]!=a[i-1])
 			ans++;
 	}
