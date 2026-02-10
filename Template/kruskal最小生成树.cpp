6db89d2c1a0fdfeb07949ce15902d3d44b1ccd03
tsh_qwq <David_tsh@163.com>
2026-02-10 16:20:53 +0800

    Update kruskal最小生成树.cpp

--- Template/kruskal最小生成树.cpp
+++ Template/kruskal最小生成树.cpp
@@ -2,27 +2,22 @@
 #define int long long
 #define endl "\n"
 using namespace std;
-struct no
-{
+struct no{
 	int x,y,z;
 }e[2000006];
 int n,m,cnt;
 int f[100005];
 int res;
-int find(int x)
-{
+int find(int x){
 	if(f[x]==x)return x;
 	return f[x]=find(f[x]);
 }
-bool cmp(no a,no b)
-{
+bool cmp(no a,no b){
 	return a.z<b.z;
 }
-void kruskal()
-{
+void kruskal(){
 	
-	for(int i=1;i<=m;i++)
-	{
+	for(int i=1;i<=m;i++){
 		int ra=find(e[i].x);
 		int rb=find(e[i].y);
 		if(ra==rb)continue;
@@ -32,14 +27,11 @@ void kruskal()
 		if(cnt==n-1)break;
 	}
 } 
-signed main()
-{
+signed main(){
 	cin>>n>>m;
 	for(int i=1;i<=n;i++)f[i]=i;
 	for(int i=1;i<=m;i++)
-	{
 		cin>>e[i].x>>e[i].y>>e[i].z;
-	}
 	sort(e+1,e+m+1,cmp);
 	kruskal();
 	if(cnt!=n-1)cout<<"orz";
